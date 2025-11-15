ccc363bbefc94fca6c9826bc78deda5dc0531f65
jientara2019cabral-dot <jientara2019cabral@gmail.com>
2025-11-14 22:37:47 -0300

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 @echo off
-echo Compilando servidor...
-gcc server.c -o ../bin/server.exe -lws2_32 -I../shared
+echo Compilando cliente...
+gcc client.c -o ../bin/client.exe -lws2_32 -I../shared
 if %ERRORLEVEL% EQU 0 (
-    echo ✓ Servidor compilado com sucesso!
+    echo ✓ Cliente compilado com sucesso!
 ) else (
-    echo ❌ Erro na compilação do servidor!
+    echo ❌ Erro na compilação do cliente!
 )
 pause
